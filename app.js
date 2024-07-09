d01ea6f0d143f2b594e197db550f4e12fb92809b
SAU RAV <ravsau00@gmail.com>
2024-07-09 09:47:48 +0545

    Day-3

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const connectToDb = require("./database/databaseConnection");
 const Blog = require("./model/blogModel");
 const app=express();
 
+const {multer,storage}=require('./middleware/multerConfig');
+const upload=multer({storage:storage});
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 connectToDb();
 
+app.use(express.static("./images"))
 app.set('view engine','ejs')
 
-app.get("/",(req,res)=>{
-  res.send("<h1>This is Homepage</h1>")
+app.get("/",async (req,res)=>{
+  const blogs=await Blog.find()    //return array
+  res.render("home.ejs",{blogs:blogs})
 })
 
 app.get("/about",(req,res)=>{
@@ -27,14 +32,18 @@ app.get("/createblog",(req,res)=>{
   res.render("createblog.ejs")
 })
 
-app.post("/createblog",async (req,res)=>{
-  console.log(req.body)
+
+app.post("/createblog",upload.single('image'),async (req,res)=>{
+  // console.log(req.body)
+  const fileName=req.file.filename;
+  
   const{title,subtitle,description}=req.body;
   console.log(title,subtitle,description);
   await Blog.create({
     title:title,
     subtitle:subtitle,
     description:description,
+    image:fileName
   })
   res.send("blog created successfully")
 })
